f1d44f1840cef30cfaa56cef083c463caf091fa4
mimu <mimu@mimu-mbp.local>
2020-10-13 16:11:35 +0800

    update

--- springboot-demo/src/main/java/com/mimu/springboot/demo/config/customizer/servlet/AppTomcatServletCustomer.java
+++ springboot-demo/src/main/java/com/mimu/springboot/demo/config/customizer/servlet/AppTomcatServletCustomer.java
@@ -3,9 +3,12 @@ package com.mimu.springboot.demo.config.customizer.servlet;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
-public class AppTomcatServletCustomer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+/**
+ * 与 tomcat servlet 容器 紧耦合 需依赖 tomcat-starter
+ */
+/*public class AppTomcatServletCustomer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
         factory.addEngineValves(new AppTomcatValveCustomer());
     }
-}
+}*/

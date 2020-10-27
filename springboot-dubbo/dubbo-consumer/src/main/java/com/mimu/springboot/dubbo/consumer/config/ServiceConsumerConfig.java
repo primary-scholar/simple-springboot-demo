e52be1bafb97cdb81172d5a326020d23635c5c01
mi mu <mimu@mimu-mini.local>
2020-10-27 22:44:54 +0800

    update

--- springboot-dubbo/dubbo-consumer/src/main/java/com/mimu/springboot/dubbo/consumer/config/ServiceConsumerConfig.java
+++ springboot-dubbo/dubbo-consumer/src/main/java/com/mimu/springboot/dubbo/consumer/config/ServiceConsumerConfig.java
@@ -1,7 +1,9 @@
 package com.mimu.springboot.dubbo.consumer.config;
 
 import com.mimu.springboot.dubbo.consumer.service.CommonService;
+import org.apache.dubbo.config.MetricsConfig;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,4 +15,11 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackageClasses = CommonService.class)
 @EnableDubbo(scanBasePackageClasses = CommonService.class)
 public class ServiceConsumerConfig {
+    @Bean
+    public MetricsConfig metricsConfig() {
+        MetricsConfig config = new MetricsConfig();
+        config.setProtocol("dubbo");
+        config.setPort("20880");
+        return config;
+    }
 }
--- springboot-dubbo/dubbo-provider/src/main/java/com/mimu/springboot/dubbo/provider/config/ServiceProviderConfig.java
+++ springboot-dubbo/dubbo-provider/src/main/java/com/mimu/springboot/dubbo/provider/config/ServiceProviderConfig.java
@@ -1,7 +1,9 @@
 package com.mimu.springboot.dubbo.provider.config;
 
 import com.mimu.springboot.dubbo.provider.service.dubbo.SchoolDataApiImpl;
+import org.apache.dubbo.config.MetricsConfig;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -9,6 +11,15 @@ import org.springframework.context.annotation.Configuration;
  * date: 2019/8/18
  */
 @Configuration
+//@ImportResource(locations = "classpath:dubbo.xml")
 @EnableDubbo(scanBasePackageClasses = SchoolDataApiImpl.class)
 public class ServiceProviderConfig {
+
+    @Bean
+    public MetricsConfig metricsConfig() {
+        MetricsConfig config = new MetricsConfig();
+        config.setProtocol("dubbo");
+        config.setPort("20880");
+        return config;
+    }
 }

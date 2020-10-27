b9d0036bda7b040fb23e596a17b3c9de6fe109ea
mimu <mimu@mimu-mbp.local>
2020-10-27 20:41:45 +0800

    update dubbo

--- springboot-dubbo/dubbo-consumer/src/main/java/com/mimu/springboot/dubbo/consumer/config/ServiceConsumerConfig.java
+++ springboot-dubbo/dubbo-consumer/src/main/java/com/mimu/springboot/dubbo/consumer/config/ServiceConsumerConfig.java
@@ -1,12 +1,7 @@
 package com.mimu.springboot.dubbo.consumer.config;
 
 import com.mimu.springboot.dubbo.consumer.service.CommonService;
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ConsumerConfig;
-import org.apache.dubbo.config.MonitorConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
-import org.springframework.context.annotation.Bean;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,37 +11,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ComponentScan(basePackageClasses = CommonService.class)
-@DubboComponentScan(basePackageClasses = CommonService.class)
+@EnableDubbo(scanBasePackageClasses = CommonService.class)
 public class ServiceConsumerConfig {
-
-    @Bean
-    public ApplicationConfig applicationConfig() {
-        ApplicationConfig applicationConfig = new ApplicationConfig();
-        applicationConfig.setName("dubbo-service-consumer");
-        return applicationConfig;
-    }
-
-    @Bean
-    public ConsumerConfig consumerConfig() {
-        ConsumerConfig consumerConfig = new ConsumerConfig();
-        consumerConfig.setTimeout(3000);
-        consumerConfig.setRetries(0);
-        return consumerConfig;
-    }
-
-    @Bean
-    public RegistryConfig registryConfig() {
-        RegistryConfig registryConfig = new RegistryConfig();
-        registryConfig.setAddress("zookeeper://localhost:2181");
-        registryConfig.setClient("curator");
-        return registryConfig;
-    }
-
-    @Bean
-    public MonitorConfig monitorConfig() {
-        MonitorConfig config = new MonitorConfig();
-        config.setProtocol("dubbo");
-        return config;
-    }
-
 }
